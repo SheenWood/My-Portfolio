cb1a72504299705a9736439b5965b0831596b1a4
SheenWood <76006571+SheenWood@users.noreply.github.com>
2021-03-31 09:50:37 -0500

    Update level01.js

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -27,10 +27,73 @@ var level01 = function (window) {
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
+        //Obstacle creation
+        function createSawBlade (x, y) {
+        var hitZoneSize = 25;
+        var damageFromObstacle = 10;
+        var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+        sawBladeHitZone.x = x;
+        sawBladeHitZone.y = y;
+        game.addGameItem(sawBladeHitZone);    
+        var obstacleImage = draw.bitmap('img/sawblade.png');
+        obstacleImage.x = -25;
+        obstacleImage.y = -25;
+        sawBladeHitZone.addChild(obstacleImage);
+        }
 
         
         
-        
+        function createEnemy (x, y) {
+        var enemy = game.createGameItem('enemy',25);
+        var redSquare = draw.rect(50,50,'red');
+        redSquare.x = -25;
+        redSquare.y = -25;
+        enemy.addChild(redSquare);
+        enemy.x = x;
+        enemy.y = y;
+        game.addGameItem(enemy);
+        enemy.velocityX = -1;
+        rotationVelocity = 10;
+
+            enemy.onPlayerCollision = function() {
+                game.changeIntegrity(-10);
+            };
+            enemy.onProjectileCollision = function() {
+                enemy.fadeOut();
+            game.increaseScore(100);
+            }
+        }
+
+        function createReward (x, y) {
+        var reward = game.createGameItem('reward',25);
+        var greenSquare = draw.rect(50,50,'green');
+        greenSquare.x = -25;
+        greenSquare.y = -25;
+        reward.addChild(greenSquare);
+        reward.x = x;
+        reward.y = y;
+        game.addGameItem(reward);
+        reward.velocityX = -0.75;
+        rotationVelocity = 10;
+
+            reward.onPlayerCollision = function() {
+                game.changeIntegrity(100);
+                game.increaseScore(100);
+                reward.fadeOut();
+            }
+        }
+        //functions calls
+        createSawBlade(2000, 200);
+        createSawBlade(500, 250);
+        createSawBlade(3000, 300); 
+ createSawBlade(500, 250);
+         createSawBlade(550, 250); createSawBlade(600, 250);
+         createSawBlade(560, 250); createSawBlade(650, 250); createSawBlade(690, 250); createSawBlade(700, 250); createSawBlade(711, 250); createSawBlade(755, 250);
+    
+
+        createEnemy(700, 300);
+
+        createReward(3000, 300);
         // DO NOT EDIT CODE BELOW HERE
     }
 };
