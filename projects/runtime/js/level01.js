28e9cf2087f20487a19c7ba68113235e8b599be3
SheenWood <76006571+SheenWood@users.noreply.github.com>
2021-03-31 10:02:09 -0500

    Update level01.js

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -87,14 +87,14 @@ var level01 = function (window) {
         createSawBlade(500, 250);
         createSawBlade(3000, 300); 
  createSawBlade(500, 250);
-         createSawBlade(550, 250); 
+         createSawBlade(2000, 250); 
         createSawBlade(600, 250);
-         createSawBlade(560, 250); 
-        createSawBlade(650, 250); 
-        createSawBlade(690, 250);
-        createSawBlade(700, 250); 
-        createSawBlade(711, 250); 
-        createSawBlade(755, 250);
+         createSawBlade(6560, 250); 
+        createSawBlade(1650, 250); 
+        createSawBlade(2690, 250);
+        createSawBlade(3700, 250); 
+        createSawBlade(4711, 250); 
+        createSawBlade(5755, 250);
     
 
         createEnemy(700, 300);
