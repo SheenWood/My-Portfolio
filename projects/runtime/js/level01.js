e6bea6d9bd8c43f491bb17ff16ffb18dc51551bd
SheenWood <76006571+SheenWood@users.noreply.github.com>
2021-03-31 11:50:59 -0500

    Update level01.js

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -83,22 +83,77 @@ var level01 = function (window) {
             }
         }
         //functions calls
-        createSawBlade(2000, 100);
-        createSawBlade(500, 150);
+        createSawBlade(2000, 200);
+        createSawBlade(500, 350);
         createSawBlade(3000, 350); 
  createSawBlade(500, 150);
-         createSawBlade(2000, 150); 
-        createSawBlade(600, 150);
-         createSawBlade(6560, 550); 
-        createSawBlade(1650, 650); 
-        createSawBlade(2690, 150);
-        createSawBlade(3700, 150); 
+         createSawBlade(2000, 350); 
+        createSawBlade(600, 350);
+         createSawBlade(6560, 350); 
+        createSawBlade(1650, 350); 
+        createSawBlade(2690, 350);
+        createSawBlade(3700, 350); 
         createSawBlade(4711, 450); 
-        createSawBlade(5755, 750);
+        createSawBlade(5755, 350);
     
 
         createEnemy(700, 300);
-
+        createEnemy(701, 300);
+        createEnemy(702, 300);
+        createEnemy(703, 300);
+        createEnemy(704, 300);
+        createEnemy(705, 300);
+        createEnemy(706, 300);
+        createEnemy(707, 300);
+        createEnemy(708, 300);
+        createEnemy(709, 300);
+        createEnemy(710, 300);
+        createEnemy(711, 300);
+        createEnemy(712, 300);
+        createEnemy(713, 300);
+        createEnemy(714, 300);
+        createEnemy(715, 300);
+        createEnemy(716, 300);
+        createEnemy(717, 300);
+        createEnemy(718, 300);
+        createEnemy(719, 300);
+        createEnemy(720, 300);
+        createEnemy(721, 300);
+        createEnemy(722, 300);
+        createEnemy(723, 300);
+        createEnemy(724, 300);
+        createEnemy(725, 300);
+        createEnemy(726, 300);
+        createEnemy(727, 300);
+        createEnemy(728, 300);
+        createEnemy(729, 300);
+        createEnemy(730, 300);
+        createEnemy(731, 300);
+        createEnemy(732, 300);
+        createEnemy(733, 300);
+        createEnemy(734, 300);
+        createEnemy(735, 300);
+        createEnemy(736, 300);
+        createEnemy(737, 300);
+        createEnemy(738, 300);
+        createEnemy(739, 300);
+        createEnemy(740, 300);
+        createEnemy(741, 300);
+        createEnemy(742, 300);
+        createEnemy(743, 300);
+        createEnemy(744, 300);
+        createEnemy(745, 300);
+        createEnemy(746, 300);
+        createEnemy(747, 300);
+        createEnemy(748, 300);
+        createEnemy(749, 300);
+        createEnemy(750, 300);
+        createEnemy(751, 300);
+        createEnemy(752, 300);
+        createEnemy(753, 300);
+        createEnemy(754, 300);
+        
+        createReward(770, 300);
         createReward(3000, 300);
         // DO NOT EDIT CODE BELOW HERE
     }
